d63254324a708a0d12e1e5f01902249618437359
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-10 16:46:07 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,7 @@ class Board
   attr_accessor :cells
   
   def initialize
-    @cells = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 end
 
 def reset!
